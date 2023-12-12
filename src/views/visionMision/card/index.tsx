bf35fb63eb90f668b33d593de3833f57146da934
David Solis <davidsolisdev@gmail.com>
2023-12-12 12:58:20 -0600

    v1

--- src/views/visionMision/card/index.tsx
+++ src/views/visionMision/card/index.tsx
@@ -9,7 +9,7 @@ interface CardVisionMisionProps {
 function CardVisionMision({ icon, title, description }: CardVisionMisionProps) {
   return (
     <article
-      className="w-[45%] h-[500px] flex flex-col flex-wrap justify-star items-start bg-[#f5f5f5] rounded-lg shadow-lg max-[905px]:w-[90%] max-[905px]:mt-7 max-[905px]:h-[400px] max-[905px]:text-sm max-[550px]:text-[0.83rem] max-[550px]:h-[450px]"
+      className="w-[45%] h-[500px] flex flex-col flex-wrap justify-star items-start bg-[#f5f5f5] rounded-lg shadow-lg max-[905px]:w-[90%] max-[905px]:mt-7 max-[905px]:h-[400px] max-[905px]:text-sm max-[550px]:text-[0.83rem] max-[550px]:h-[470px]"
       data-aos={title === "Misión" ? "flip-right" : "flip-left"}
     >
       <div className="h-[200px] mt-14 w-full flex flex-wrap justify-center">
