541445be797cae206cec6d822594f6e52f975468
David Solis <davidsolisdev@gmail.com>
2023-12-12 12:55:54 -0600

    v1

--- src/components/carrusel/carruselTexts/carruselText.tsx
+++ src/components/carrusel/carruselTexts/carruselText.tsx
@@ -15,16 +15,16 @@ function CarruselText({
 }: CarruselTextProps) {
   return (
     <section className="w-[600px] absolute left-[100px] max-[945px]:left-[50px] animate__animated animate__backInUp">
-      <div className="w-full flex flex-col">
-        <span className="w-full text-5xl font-bold text-red-700">
+      <div className="w-full flex flex-wrap flex-col">
+        <span className="w-full text-5xl max-[500px]:text-4xl font-bold text-red-700">
           {textRed}
         </span>
-        <span className="w-full text-5xl font-bold text-gray-500 mt-2">
+        <span className="w-full text-5xl max-[500px]:text-4xl font-bold text-gray-500 mt-2">
           {textGray}
         </span>
         <span className="w-[30%] h-[10px] bg-red-700"></span>
       </div>
-      <p className="text-xl mt-5 text-gray-500">{description}</p>
+      <p className="text-xl max-[500px]:text-base mt-5 text-gray-500">{description}</p>
       <p className="text-base text-red-700">{descriptionRed}</p>
     </section>
   );
